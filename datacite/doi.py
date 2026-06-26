d233199b5c2584eba83580cd3c0cf379a051507c
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2026-06-26 22:51:09 +0200

    Fix: it may legitimately happen in Doi.update() that there is nothing to do

--- datacite/doi.py
+++ datacite/doi.py
@@ -161,7 +161,8 @@ class Doi:
                 self._init_data()
             self._data['data']['attributes']['event'] = event
         elif self._data is None:
-            raise ValueError("DOI attributes not set")
+            # nothing to do
+            return
         headers = {'content-type': 'application/vnd.api+json'}
         response = requests.put(config.apiurl+self.doi,
                                 data=json.dumps(self._data),
